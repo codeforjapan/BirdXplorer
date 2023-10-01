f21e58f77ffc1e7be119d2dd8e7a32453c47e3c8
osoken <osoken@osoekawalab.com>
2023-10-01 12:38:37 +0900

    fix(main): add missing revisions

--- birdxplorer/app.py
+++ birdxplorer/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from .logger import get_logger
+from .routers.system import gen_router as gen_system_router
 from .settings import GlobalSettings
 
 
 def gen_app(settings: GlobalSettings) -> FastAPI:
     _ = get_logger(level=settings.logger_settings.level)
     app = FastAPI()
+    app.include_router(gen_system_router())
     return app
