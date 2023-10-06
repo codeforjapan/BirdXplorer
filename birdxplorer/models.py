65979c09d57bd60ae57ca3b3d5cc532f64ec7e6c
osoken <osoken@osoekawalab.com>
2023-10-06 22:12:23 +0900

    feat(models): add NoteId type

--- birdxplorer/models.py
+++ birdxplorer/models.py
@@ -1,11 +1,77 @@
 from datetime import datetime, timezone
-from typing import TypeAlias
+from typing import Any, Type, TypeAlias, TypeVar
 
 from pydantic import BaseModel as PydanticBaseModel
-from pydantic import ConfigDict, Field
+from pydantic import ConfigDict, Field, GetCoreSchemaHandler, TypeAdapter
 from pydantic.alias_generators import to_camel
+from pydantic_core import core_schema
 
 IncEx: TypeAlias = "set[int] | set[str] | dict[int, IncEx] | dict[str, IncEx] | None"
+StrT = TypeVar("StrT", bound="BaseString")
+
+
+class BaseString(str):
+    """
+    >>> BaseString("test")
+    BaseString('test')
+    >>> str(BaseString("test"))
+    'test'
+    >>> ta = TypeAdapter(BaseString)
+    >>> ta.validate_python("test")
+    BaseString('test')
+    >>> ta.validate_python(1)
+    Traceback (most recent call last):
+     ...
+    pydantic_core._pydantic_core.ValidationError: 1 validation error for function-after[validate(), str]
+      Input should be a valid string [type=string_type, input_value=1, input_type=int]
+     ...
+    >>> ta.dump_json(BaseString("test_test"))
+    b'"test_test"'
+    >>> BaseString.from_str("test")
+    BaseString('test')
+    >>> ta.validate_python(BaseString.from_str("test"))
+    BaseString('test')
+    >>> hash(BaseString("test")) == hash("test")
+    True
+    >>> ta.validate_python("test　test")
+    BaseString('test　test')
+    """
+
+    @classmethod
+    def _proc_str(cls, s: str) -> str:
+        return s
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({super().__repr__()})"
+
+    def __str__(self) -> str:
+        return super(BaseString, self).__str__()
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type: Any, _handler: GetCoreSchemaHandler) -> core_schema.CoreSchema:
+        return core_schema.no_info_after_validator_function(
+            cls.validate,
+            core_schema.str_schema(**cls.__get_extra_constraint_dict__()),
+            serialization=core_schema.plain_serializer_function_ser_schema(cls.serialize, when_used="json"),
+        )
+
+    @classmethod
+    def validate(cls: Type[StrT], v: Any) -> StrT:
+        return cls(cls._proc_str(v))
+
+    def serialize(self) -> str:
+        return str(self)
+
+    @classmethod
+    def __get_extra_constraint_dict__(cls) -> dict[str, Any]:
+        return {}
+
+    def __hash__(self) -> int:
+        return super(BaseString, self).__hash__()
+
+    @classmethod
+    def from_str(cls: Type[StrT], v: str) -> StrT:
+        return TypeAdapter(cls).validate_python(v)
 
 
 class BaseModel(PydanticBaseModel):
@@ -74,8 +140,25 @@ class UserEnrollment(BaseModel):
     modeling_group: str
 
 
+class NoteId(BaseString):
+    """
+    >>> NoteId.from_str("test")
+    Traceback (most recent call last):
+     ...
+    pydantic_core._pydantic_core.ValidationError: 1 validation error for function-after[validate(), constrained-str]
+      String should match pattern '^[0-9]{19}$' [type=string_pattern_mismatch, input_value='test', input_type=str]
+     ...
+    >>> NoteId.from_str("1234567890123456789")
+    NoteId('1234567890123456789')
+    """
+
+    @classmethod
+    def __get_extra_constraint_dict__(cls) -> dict[str, Any]:
+        return dict(super().__get_extra_constraint_dict__(), pattern=r"^[0-9]{19}$")
+
+
 class Notes(BaseModel):
-    note_id: str = Field(pattern=r"^[0-9]{19}$")
+    note_id: NoteId
     note_author_participant_id: str = Field(pattern=r"^[0-9A-F]{64}$")
     created_at_millis: int = Field(
         gt=(int(datetime(2006, 7, 15, 0, 0, 0, 0, timezone.utc).timestamp() * 1000)),
