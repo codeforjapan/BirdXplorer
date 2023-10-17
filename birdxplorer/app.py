3fee7dd67802a827757a657fce04d175af3ad1e1
osoken <osoken@osoekawalab.com>
2023-10-17 23:20:41 +0900

    fix(app): fix ping route

--- birdxplorer/app.py
+++ birdxplorer/app.py
@@ -8,5 +8,5 @@ from .settings import GlobalSettings
 def gen_app(settings: GlobalSettings) -> FastAPI:
     _ = get_logger(level=settings.logger_settings.level)
     app = FastAPI()
-    app.include_router(gen_system_router())
+    app.include_router(gen_system_router(), prefix="/api/v1/system")
     return app
--- tests/routers/test_system.py
+++ tests/routers/test_system.py
@@ -2,6 +2,6 @@ from fastapi.testclient import TestClient
 
 
 def test_ping(client: TestClient) -> None:
-    response = client.get("/ping")
+    response = client.get("/api/v1/system/ping")
     assert response.status_code == 200
     assert response.json() == {"message": "pong"}
