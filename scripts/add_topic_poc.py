38a0f170b77d24d4d71c51ea01af543e851fc318
kota-yata <kota@yatagai.com>
2023-12-11 15:32:42 +0900

    modify prompt

--- scripts/add_topic_poc.py
+++ scripts/add_topic_poc.py
@@ -6,8 +6,16 @@ from typing import Dict, List
 from dotenv import load_dotenv
 from openai import OpenAI
 
+response_sample = """
+{
+  "1700958646329": {
+    "topics": ["医療, 福祉"...],
+    "language": "en"
+  }
+}
+"""
 
-def get_topic(client: OpenAI, tweet: str, note: str) -> Dict[str, List[str]]:
+def get_topic(client: OpenAI, note_id: int, tweet: str, note: str) -> Dict[str, List[str]]:
     with open(os.path.join(os.path.dirname(__file__), "fewshot_sample.json"), "r") as f:
         fewshot_sample = json.load(f)
 
@@ -25,18 +33,18 @@ def get_topic(client: OpenAI, tweet: str, note: str) -> Dict[str, List[str]]:
 {fewshot_sample["note"]}
 ```
 このセットに対してのトピックは「{" ".join(fewshot_sample["topics"])}」です。
-これを踏まえて、以下のツイートとコミュニティノートに対して同じ粒度で複数のトピックを提示してください。形式はJSONで、キーをtopicsとして値にトピックを配列で格納してください。
-レスポンスの例:
+これを踏まえて、以下のセットに対して同じ粒度で複数のトピック(少なくとも3つ)を提示してください。形式はJSONで、キーをtopicsとして値にトピックを配列で格納してください。また、ツイートに用いられている言語も推定し、キーをlanguageとしてiso 639-1に準拠した言語コードを格納してください。topicsとlanguageを格納するオブジェクトはnote idをキーとした値に格納してください
+レスポンスの例 (1700958646329はnote id):
 ```
-{
-  "topics": ["医療", "福祉", ...]
-}
+{response_sample}
 ```
 """,
             },
             {
                 "role": "user",
-                "content": f"""ツイート:
+                "content": f"""
+note id: {note_id}
+ツイート:
 ```
 {tweet}
 ```
@@ -56,19 +64,22 @@ def get_topic(client: OpenAI, tweet: str, note: str) -> Dict[str, List[str]]:
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    # parser.add_argument("input_file")
+    parser.add_argument("input_file")
     # parser.add_argument("output_file")
     args = parser.parse_args()
     load_dotenv()
     client = OpenAI()
-    # with open(args.input_file, "r", encoding="utf-8") as f:
-    #     tweets = json.load(f)
-    print(
-        get_topic(
-            client,
-            """Tweet content goes here.
-""",
-            """Community note goes here
-""",
+    with open(args.input_file, "r", encoding="utf-8") as f:
+        notes = json.load(f)
+    # create output json file with topics. make the file an array of objects which is each response
+    with open("data/output.json", "w", encoding="utf-8") as f:
+        json.dump(
+            [
+                get_topic(client, note["noteId"], note["tweetBody"], note["noteBody"])
+                for note in notes
+            ],
+            f,
+            ensure_ascii=False,
+            indent=2,
         )
-    )
+    
