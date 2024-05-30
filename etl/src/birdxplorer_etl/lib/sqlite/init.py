68ba3ed2b22f527b355800697eadfdbb46c19dcc
osoken <osoken@osoekawalab.com>
2024-05-30 20:58:12 +0900

    fix(etl): remove redundant codes

--- etl/src/birdxplorer_etl/lib/sqlite/init.py
+++ etl/src/birdxplorer_etl/lib/sqlite/init.py
@@ -5,32 +5,13 @@ from prefect import get_run_logger
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 
-from birdxplorer_common.storage import Base, RowNoteRecord
+from birdxplorer_common.storage import RowNoteRecord
 
 
 def init_db():
     logger = get_run_logger()
 
     # ToDo: dbファイルをS3など外部に置く必要がある。
-    db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "data", "note.db"))
-    logger.info(f"Initializing database at {db_path}")
-    engine = create_engine("sqlite:///" + db_path)
-
-    # 一時データベースのテーブル作成する
-    # ToDo: noteテーブル以外に必要なものを追加
-    if not inspect(engine).has_table("note"):
-        logger.info("Creating table note")
-        RowNoteRecord.metadata.create_all(engine)
-
-    db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "data", "note.db"))
-    logger.info(f"Initializing database at {db_path}")
-    engine = create_engine("sqlite:///" + db_path)
-    Base.metadata.create_all(engine)
-
-    # 一時データベースのテーブル作成する
-    # ToDo: noteテーブル以外に必要なものを追加
-    if not inspect(engine).has_table("note"):
-        logger.info("Creating table note")
     db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "data", "note.db"))
     logger.info(f"Initializing database at {db_path}")
     engine = create_engine("sqlite:///" + db_path)
