f400bc2f93f2fab0eb353ad5a254b38cbed4e72f
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-12-23 14:33:10 +0900

    search mock

--- api/birdxplorer_api/routers/data.py
+++ api/birdxplorer_api/routers/data.py
@@ -10,6 +10,7 @@ from typing_extensions import Annotated
 from birdxplorer_api.openapi_doc import (
     V1DataNotesDocs,
     V1DataPostsDocs,
+    V1DataSearchDocs,
     V1DataTopicsDocs,
     V1DataUserEnrollmentsDocs,
 )
@@ -22,6 +23,7 @@ from birdxplorer_common.models import (
     ParticipantId,
     Post,
     PostId,
+    SummaryString,
     Topic,
     TopicId,
     TwitterTimestamp,
@@ -49,6 +51,18 @@ NotesPaginationMetaWithExamples: TypeAlias = Annotated[
         json_schema_extra={
             "examples": [
                 {"next": "http://birdxplorer.onrender.com/api/v1/data/notes?offset=100&limit=100", "prev": "null"}
+            ]
+        },
+    ),
+]
+
+SearchPaginationMetaWithExamples: TypeAlias = Annotated[
+    PaginationMeta,
+    PydanticField(
+        description="ページネーション用情報。 リクエスト時に指定した offset / limit の値に応じて、次のページや前のページのリクエスト用 URL が設定される。",
+        json_schema_extra={
+            "examples": [
+                {"next": "http://birdxplorer.onrender.com/api/v1/data/search?offset=100&limit=100", "prev": "null"}
             ]
         },
     ),
@@ -141,6 +155,92 @@ PostListWithExamples: TypeAlias = Annotated[
 ]
 
 
+class SearchedNote(BaseModel):
+    noteId: NoteId
+    summary: Annotated[SummaryString, PydanticField(description="コミュニティノートの本文")]
+    language: Annotated[LanguageIdentifier, PydanticField(description="コミュニティノートの言語")]
+    topics: Annotated[List[Topic], PydanticField(description="コミュニティノートに関連付けられたトピックのリスト")]
+    postId: PostId
+    current_status: Annotated[
+        Annotated[
+            str,
+            PydanticField(
+                json_schema_extra={
+                    "enum": ["NEEDS_MORE_RATINGS", "CURRENTLY_RATED_HELPFUL", "CURRENTLY_RATED_NOT_HELPFUL"]
+                },
+            ),
+        ]
+        | None,
+        PydanticField(
+            description="コミュニティノートの現在の評価状態",
+        ),
+    ]
+    created_at: Annotated[
+        TwitterTimestamp, PydanticField(description="コミュニティノートの作成日時 (ミリ秒単位の UNIX EPOCH TIMESTAMP)")
+    ]
+    post: Annotated[Post, PydanticField(description="コミュニティノートに関連付けられた Post の情報")]
+
+
+SearchWithExamples: TypeAlias = Annotated[
+    List[SearchedNote],
+    PydanticField(
+        description="検索結果のノートのリスト",
+        json_schema_extra={
+            "examples": [
+                {
+                    "noteId": "1845672983001710655",
+                    "language": "ja",
+                    "topics": [
+                        {
+                            "topicId": 26,
+                            "label": {"ja": "セキュリティ上の脅威", "en": "security threat"},
+                            "referenceCount": 0,
+                        },
+                        {"topicId": 47, "label": {"ja": "検閲", "en": "Censorship"}, "referenceCount": 0},
+                        {"topicId": 51, "label": {"ja": "テクノロジー", "en": "technology"}, "referenceCount": 0},
+                    ],
+                    "summary": "Content Security Policyは情報の持ち出しを防止する仕組みではありません。コンテンツインジェクションの脆弱性のリスクを軽減する仕組みです。適切なContent Security Policyがレスポンスヘッダーに設定されている場合でも、外部への通信をブロックできない点に注意が必要です。    Content Security Policy Level 3  https://w3c.github.io/webappsec-csp/",  # noqa: E501
+                    "currentStatus": "NEEDS_MORE_RATINGS",
+                    "createdAt": 1728877704750,
+                    "post": {
+                        "postId": "1846718284369912064",
+                        "xUserId": "90954365",
+                        "xUser": {
+                            "userId": "90954365",
+                            "name": "earthquakejapan",
+                            "profileImage": "https://pbs.twimg.com/profile_images/1638600342/japan_rel96_normal.jpg",
+                            "followersCount": 162934,
+                            "followingCount": 6,
+                        },
+                        "text": "今後48時間以内に日本ではマグニチュード6.0の地震が発生する可能性があります。地図をご覧ください。（10月17日～10月18日） - https://t.co/nuyiVdM4FW https://t.co/Xd6U9XkpbL",  # noqa: E501
+                        "mediaDetails": [
+                            {
+                                "mediaKey": "3_1846718279236177920-1846718284369912064",
+                                "type": "photo",
+                                "url": "https://pbs.twimg.com/media/GaDcfZoX0AAko2-.jpg",
+                                "width": 900,
+                                "height": 738,
+                            }
+                        ],
+                        "createdAt": 1729094524000,
+                        "likeCount": 451,
+                        "repostCount": 104,
+                        "impressionCount": 82378,
+                        "links": [
+                            {
+                                "linkId": "9c139b99-8111-e4f0-ad41-fc9e40d08722",
+                                "url": "https://www.quakeprediction.com/Earthquake%20Forecast%20Japan.html",
+                            }
+                        ],
+                        "link": "https://x.com/earthquakejapan/status/1846718284369912064",
+                    },
+                },
+            ]
+        },
+    ),
+]
+
+
 class TopicListResponse(BaseModel):
     data: TopicListWithExamples
 
@@ -153,6 +253,11 @@ class NoteListResponse(BaseModel):
 class PostListResponse(BaseModel):
     data: PostListWithExamples
     meta: PostsPaginationMetaWithExamples
+
+
+class SearchResponse(BaseModel):
+    data: SearchWithExamples
+    meta: SearchPaginationMetaWithExamples
 
 
 def str_to_twitter_timestamp(s: str) -> TwitterTimestamp:
@@ -309,5 +414,95 @@ def gen_router(storage: Storage) -> APIRouter:
             prev_url = f"{base_url}?offset={prev_offset}&limit={limit}"
 
         return PostListResponse(data=posts, meta=PaginationMeta(next=next_url, prev=prev_url))
+
+    @router.get("/search", description=V1DataSearchDocs.description, response_model=SearchResponse)
+    def search(
+        note_includes_text: Union[None, str] = Query(default=None, **V1DataSearchDocs.params["note_includes_text"]),
+        note_excludes_text: Union[None, str] = Query(default=None, **V1DataSearchDocs.params["note_excludes_text"]),
+        post_includes_text: Union[None, str] = Query(default=None, **V1DataSearchDocs.params["post_includes_text"]),
+        post_excludes_text: Union[None, str] = Query(default=None, **V1DataSearchDocs.params["post_excludes_text"]),
+        language: Union[LanguageIdentifier, None] = Query(default=None, **V1DataSearchDocs.params["language"]),
+        topic_ids: Union[List[TopicId], None] = Query(default=None, **V1DataSearchDocs.params["topic_ids"]),
+        note_status: Union[None, List[str]] = Query(default=None, **V1DataSearchDocs.params["note_status"]),
+        note_created_at_from: Union[None, TwitterTimestamp, str] = Query(
+            default=None, **V1DataSearchDocs.params["note_created_at_from"]
+        ),
+        note_created_at_to: Union[None, TwitterTimestamp, str] = Query(
+            default=None, **V1DataSearchDocs.params["note_created_at_to"]
+        ),
+        x_user_names: Union[List[str], None] = Query(default=None, **V1DataSearchDocs.params["x_user_name"]),
+        x_user_followers_count_from: Union[None, int] = Query(
+            default=None, **V1DataSearchDocs.params["x_user_followers_count_from"]
+        ),
+        x_user_follow_count_from: Union[None, int] = Query(
+            default=None, **V1DataSearchDocs.params["x_user_follow_count_from"]
+        ),
+        post_favorite_count_from: Union[None, int] = Query(
+            default=None, **V1DataSearchDocs.params["post_favorite_count_from"]
+        ),
+        post_repost_count_from: Union[None, int] = Query(
+            default=None, **V1DataSearchDocs.params["post_repost_count_from"]
+        ),
+        post_impression_count_from: Union[None, int] = Query(
+            default=None, **V1DataSearchDocs.params["post_impression_count_from"]
+        ),
+        post_includes_media: bool = Query(default=True, **V1DataSearchDocs.params["post_includes_media"]),
+        offset: int = Query(default=0, ge=0, **V1DataSearchDocs.params["offset"]),
+        limit: int = Query(default=100, gt=0, le=1000, **V1DataSearchDocs.params["limit"]),
+    ) -> SearchResponse:
+        return SearchResponse(
+            data=[
+                SearchedNote(
+                    noteId="1845672983001710655",
+                    language="ja",
+                    topics=[
+                        {
+                            "topicId": 26,
+                            "label": {"ja": "セキュリティ上の脅威", "en": "security threat"},
+                            "referenceCount": 0,
+                        },
+                        {"topicId": 47, "label": {"ja": "検閲", "en": "Censorship"}, "referenceCount": 0},
+                        {"topicId": 51, "label": {"ja": "テクノロジー", "en": "technology"}, "referenceCount": 0},
+                    ],
+                    postId="1846718284369912064",
+                    summary="Content Security Policyは情報の持ち出しを防止する仕組みではありません。コンテンツインジェクションの脆弱性のリスクを軽減する仕組みです。適切なContent Security Policyがレスポンスヘッダーに設定されている場合でも、外部への通信をブロックできない点に注意が必要です。    Content Security Policy Level 3  https://w3c.github.io/webappsec-csp/",  # noqa: E501
+                    current_status="NEEDS_MORE_RATINGS",
+                    created_at=1728877704750,
+                    post={
+                        "postId": "1846718284369912064",
+                        "xUserId": "90954365",
+                        "xUser": {
+                            "userId": "90954365",
+                            "name": "earthquakejapan",
+                            "profileImage": "https://pbs.twimg.com/profile_images/1638600342/japan_rel96_normal.jpg",
+                            "followersCount": 162934,
+                            "followingCount": 6,
+                        },
+                        "text": "今後48時間以内に日本ではマグニチュード6.0の地震が発生する可能性があります。地図をご覧ください。",
+                        "mediaDetails": [
+                            {
+                                "mediaKey": "3_1846718279236177920-1846718284369912064",
+                                "type": "photo",
+                                "url": "https://pbs.twimg.com/media/GaDcfZoX0AAko2-.jpg",
+                                "width": 900,
+                                "height": 738,
+                            }
+                        ],
+                        "createdAt": 1729094524000,
+                        "likeCount": 451,
+                        "repostCount": 104,
+                        "impressionCount": 82378,
+                        "links": [
+                            {
+                                "linkId": "9c139b99-8111-e4f0-ad41-fc9e40d08722",
+                                "url": "https://www.quakeprediction.com/Earthquake%20Forecast%20Japan.html",
+                            }
+                        ],
+                        "link": "https://x.com/earthquakejapan/status/1846718284369912064",
+                    },
+                )
+            ],
+            meta=PaginationMeta(next=None, prev=None),
+        )
 
     return router
